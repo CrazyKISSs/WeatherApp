a8b7b66ed0ce658e0d71059712ffb094ee12b4c8
Kristina <ajiaha@gmail.com>
2022-09-24 22:03:54 +0300

    Initial commit

---
+++ app/src/main/java/ru/krisproject/weatherapp/MainActivity.java
@@ -0,0 +1,21 @@
+package ru.krisproject.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText user_field;
+    private Button main_btn;
+    private TextView result_info;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
