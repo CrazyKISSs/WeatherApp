3e20f4c8aae04d2dad59f8eb50720cc1998b796b
Kristina <ajiaha@gmail.com>
2022-09-24 22:14:13 +0300

    2.0

--- app/src/main/java/ru/krisproject/weatherapp/MainActivity.java
+++ app/src/main/java/ru/krisproject/weatherapp/MainActivity.java
@@ -3,9 +3,11 @@ package ru.krisproject.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +19,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        user_field = findViewById(R.id.user_field);
+        main_btn = findViewById(R.id.main_btn);
+        result_info = findViewById(R.id.result_info);
+
+        main_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(user_field.getText().toString().trim().equals("")){
+                    Toast.makeText(MainActivity.this, R.string.no_user_input, Toast.LENGTH_LONG).show();
+                }
+                else {
+                    
+                }
+            }
+        });
     }
 }
