a3f59263d7e522f29cbef6b3d58d4ba0e358f87a
Kristina <ajiaha@gmail.com>
2022-09-25 14:00:21 +0300

    2.0

--- app/src/main/java/ru/krisproject/weatherapp/MainActivity.java
+++ app/src/main/java/ru/krisproject/weatherapp/MainActivity.java
@@ -2,12 +2,23 @@ package ru.krisproject.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,9 +42,70 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, R.string.no_user_input, Toast.LENGTH_LONG).show();
                 }
                 else {
-                    
+                    String city = user_field.getText().toString();
+                    String key = "2e7614bc6a36df15cdcc779e9b344f14";
+                    String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key + "&units=metric&lang=ru";
+
+                    new GetUrlData().execute(url);
                 }
             }
         });
+    }
+
+    private class GetUrlData extends AsyncTask<String, String, String >{
+
+        protected void onPreExecute(){
+            super.onPreExecute();
+            result_info.setText("Ожидайте...");
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(strings[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line  = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line).append("\n");
+                }
+                return  buffer.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                if(connection != null)
+                    connection.disconnect();
+                try {
+                    if(reader != null)
+                        reader.close();
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+
+            }
+
+            return null;
+        }
+
+        @Override
+        protected  void onPostExecute(String result){
+            super.onPostExecute(result);
+
+            result_info.setText(result);
+        }
+
     }
 }
